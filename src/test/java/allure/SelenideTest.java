74a8fccdb841f4aa3dd7177bc3070270aec09a0b
Evgeniia Miroshina <evgeniia.miroshina@t-systems.com>
2022-01-02 21:28:06 +0300

    added Listener

--- src/test/java/allure/SelenideTest.java
+++ src/test/java/allure/SelenideTest.java
@@ -1,6 +1,9 @@
 package allure;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.LogEventListener;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.withText;
@@ -14,6 +17,7 @@ public class SelenideTest {
 
     @Test
     public void TestOfIssueName() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
 
         open("https://github.com");
 

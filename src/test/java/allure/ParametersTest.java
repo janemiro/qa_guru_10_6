810c7055b91b1942f4c15df377af9f6b483e687c
Evgeniia Miroshina <evgeniia.miroshina@t-systems.com>
2022-01-07 01:39:04 +0300

    fixed AllureServe

--- src/test/java/allure/ParametersTest.java
+++ src/test/java/allure/ParametersTest.java
@@ -1,15 +1,15 @@
 package allure;
 
 import io.qameta.allure.Allure;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 
 public class ParametersTest {
-    @Disabled
     @DisplayName("Check of")
-   // @ParameterizedTest(name = "{displayName} {0}")
-    //@ValueSource(strings = {"L.A.", "Hamburg"})
+    @ParameterizedTest(name = "{displayName} {0}")
+    @ValueSource(strings = {"Los Angeles", "Berlin"})
     public void testLambda(String city) {
         Allure.parameter("City", city);
     }
